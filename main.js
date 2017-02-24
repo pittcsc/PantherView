9286e4845e21a5c74f29c8f48bd99825877a5cec
Kyle Amoroso <kamoroso1994@gamil.com>
2017-02-24 18:06:52 -0500

    Reverted back to double quotes

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 (function (window, undefined) {
-    'use strict';
+    "use strict";
 
     // Oakland Coordinates: 40.4388 N, 79.9514 W (40.4388, -79.9514)
     // Cathy Coordinates: 40° 26′ 39″ N, 79° 57′ 11″ W (40.444167, -79.953056)
     const cathyLatLong = [40.444167, -79.953056];
-    const map = L.map('mapid', {
+    const map = L.map("mapid", {
         center: cathyLatLong,
         zoom: 15,
         minZoom: 12,
@@ -12,21 +12,21 @@
         maxBoundsViscosity: 0.90
     });
 
-    const sidebar = document.getElementById('sidebar');
-    const sidebarToggle = document.getElementById('sidebarToggle');
+    const sidebar = document.getElementById("sidebar");
+    const sidebarToggle = document.getElementById("sidebarToggle");
 
     //Start with sidebar closed if mobile
     if (screen.width <= 800) {
         sidebarToggle.open = 0;
-        sidebar.className = 'hidden';
-        sidebarToggle.className = 'fa fa-chevron-right fa-3x';
+        sidebar.className = "hidden";
+        sidebarToggle.className = "fa fa-chevron-right fa-3x";
     } else {
         sidebarToggle.open = 1;
-        sidebar.className = 'shown';
-        sidebarToggle.className = 'fa fa-chevron-left fa-3x';
+        sidebar.className = "shown";
+        sidebarToggle.className = "fa fa-chevron-left fa-3x";
     }
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
@@ -125,39 +125,39 @@
     function toggleSidebar() {
         if (sidebarToggle.open == 1) {
             sidebarToggle.open = 0;
-            sidebar.className = 'hidden';
-            sidebarToggle.className = 'fa fa-chevron-right fa-3x';
+            sidebar.className = "hidden";
+            sidebarToggle.className = "fa fa-chevron-right fa-3x";
         } else {
             sidebarToggle.open = 1;
-            sidebar.className = 'shown';
-            sidebarToggle.className = 'fa fa-chevron-left fa-3x';
+            sidebar.className = "shown";
+            sidebarToggle.className = "fa fa-chevron-left fa-3x";
         }
     }
 
     //Listeners for date buttons
-    document.getElementById('radioDay').addEventListener('click', displayPastDay);
-    document.getElementById('radioWeek').addEventListener('click', displayPastWeek);
-    document.getElementById('radioMonth').addEventListener('click', displayPastMonth);
+    document.getElementById("radioDay").addEventListener("click", displayPastDay);
+    document.getElementById("radioWeek").addEventListener("click", displayPastWeek);
+    document.getElementById("radioMonth").addEventListener("click", displayPastMonth);
 
     //Listener for sidebar toggle
-    document.getElementById('sidebarToggle').addEventListener('click', toggleSidebar);
+    document.getElementById("sidebarToggle").addEventListener("click", toggleSidebar);
 
     // Display a notification to the user.
-    // Style is optional, can be 'error', 'warning', or 'success'
+    // Style is optional, can be "error", "warning", or "success"
     function displayNotification(messageText, style, customHTML) {
-        const notificationArea = document.getElementById('notifications');
-        const box = document.createElement('div');
-        box.className = 'notification';
+        const notificationArea = document.getElementById("notifications");
+        const box = document.createElement("div");
+        box.className = "notification";
 
         if (style) {
             box.classList.add(style);
         }
 
-        const closeButton = document.createElement('button');
-        closeButton.className = 'close';
-        closeButton.innerHTML = 'x';
-        closeButton.addEventListnener('click', function() {
-            box.style.display = 'none';
+        const closeButton = document.createElement("button");
+        closeButton.className = "close";
+        closeButton.innerHTML = "x";
+        closeButton.addEventListnener("click", function() {
+            box.style.display = "none";
         });
 
         box.appendChild(closeButton);
@@ -166,7 +166,7 @@
         box.appendChild(textarea);
 
         if (customHTML) {
-            const customDiv = document.createElement('div');
+            const customDiv = document.createElement("div");
             customHTML(customDiv);
             box.appendChild(customDiv);
         }
@@ -177,47 +177,47 @@
 
 
     // WPRDC data
-    const WPRDC_BASE_URL = 'https://data.wprdc.org/api/action/datastore_search_sql?sql=';
+    const WPRDC_BASE_URL = "https://data.wprdc.org/api/action/datastore_search_sql?sql=";
 
     // Marker Icons
     const iconTypes = {
         CITY_POLICE: L.divIcon({
-            className: 'map-pin blue',
+            className: "map-pin blue",
             html: '<i class="fa fa-balance-scale"></i>',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
             popupAnchor: [0, -16]
         }),
         CITY_ARREST: L.divIcon({
-            className: 'map-pin red',
+            className: "map-pin red",
             html: '<i class="fa fa-gavel"></i>',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
             popupAnchor: [0, -16]
         }),
         CITY_311_ICON: L.divIcon({
-            className: 'map-pin yellow',
+            className: "map-pin yellow",
             html: '<i class="fa fa-commenting"></i>',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
             popupAnchor: [0, -16]
         }),
         LIBRARY_ICON: L.divIcon({
-            className: 'map-pin black',
+            className: "map-pin black",
             html: '<i class="fa fa-book"></i>',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
             popupAnchor: [0, -16]
         }),
         CODE_VIOLATION: L.divIcon({
-            className: 'map-pin green',
+            className: "map-pin green",
             html: '<i class="fa fa-times-circle"></i>',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
             popupAnchor: [0, -16]
         }),
         NON_TRAFFIC_VIOLATION: L.divIcon({
-            className: 'map-pin darkorchid',
+            className: "map-pin darkorchid",
             html: '<i class="fa fa-sticky-note-o"></i>',
             iconSize: [32, 32],
             iconAnchor: [16, 32],
@@ -227,14 +227,14 @@
 
     const WPRDC_DATA_SOURCES = {
         "Police": {
-            id: '1797ead8-8262-41cc-9099-cbc8a161924b',
+            id: "1797ead8-8262-41cc-9099-cbc8a161924b",
             primaryFiltering: 'WHERE "INCIDENTNEIGHBORHOOD" LIKE \'%Oakland\'',
-            latLong: ['Y', 'X'],
+            latLong: ["Y", "X"],
             icon: iconTypes.CITY_POLICE,
 
             // TODO: Better title and popup messages?
-            title: (record) => record['OFFENSES'],
-            popup: (record) => record['OFFENSES'],
+            title: (record) => record["OFFENSES"],
+            popup: (record) => record["OFFENSES"],
 
             processRecord: (record) => {
                 // Collect time of incident from the record
@@ -245,14 +245,14 @@
         },
 
         "Arrest": {
-            id: 'e03a89dd-134a-4ee8-a2bd-62c40aeebc6f',
+            id: "e03a89dd-134a-4ee8-a2bd-62c40aeebc6f",
             primaryFiltering: 'WHERE "INCIDENTNEIGHBORHOOD" LIKE \'%Oakland\'',
-            latLong: ['Y', 'X'],
+            latLong: ["Y", "X"],
             icon: iconTypes.CITY_ARREST,
 
             // TODO: Better title and popup messages?
-            title: (record) => record['OFFENSES'],
-            popup: (record) => record['OFFENSES'],
+            title: (record) => record["OFFENSES"],
+            popup: (record) => record["OFFENSES"],
 
             processRecord: (record) => {
                 // Collect time of incident from the record
@@ -263,16 +263,16 @@
         },
 
         "Code Violation": {
-            id: '4e5374be-1a88-47f7-afee-6a79317019b4',
+            id: "4e5374be-1a88-47f7-afee-6a79317019b4",
             primaryFiltering: 'WHERE "NEIGHBORHOOD" LIKE \'%Oakland\'',
-            latLong: ['Y', 'X'],
+            latLong: ["Y", "X"],
             icon: iconTypes.CODE_VIOLATION,
 
             // TODO: Better title and popup messages?
-            title: (record) => record['VIOLATION'],
-            popup: (record) => `<strong>${record['VIOLATION']}:</strong>
-            ${record['LOCATION']}<br>
-            ${record['STREET_NUM']} ${record['STREET_NAME']}`,
+            title: (record) => record["VIOLATION"],
+            popup: (record) => `<strong>${record["VIOLATION"]}:</strong>
+            ${record["LOCATION"]}<br>
+            ${record["STREET_NUM"]} ${record["STREET_NAME"]}`,
 
             processRecord: (record) => {
                 // Collect time of incident from the record
@@ -285,15 +285,15 @@
         // City of Pittsburgh 311 data
         // TODO: would be great to prune 311 data to the last 30 days, like the police data
         "311": {
-            id: '40776043-ad00-40f5-9dc8-1fde865ff571',
+            id: "40776043-ad00-40f5-9dc8-1fde865ff571",
             primaryFiltering: 'WHERE "NEIGHBORHOOD" LIKE \'%Oakland\' ORDER BY "CREATED_ON" DESC',
-            latLong: ['Y', 'X'],
+            latLong: ["Y", "X"],
             icon: iconTypes.CITY_311_ICON,
 
-            title: (record) => record['REQUEST_TYPE'],
+            title: (record) => record["REQUEST_TYPE"],
             popup: (record) => `
-              <strong>${record['DEPARTMENT']}</strong>
-              <br> ${record['REQUEST_TYPE']}`,
+              <strong>${record["DEPARTMENT"]}</strong>
+              <br> ${record["REQUEST_TYPE"]}`,
 
             processRecord: (record) => {
                 // Collect time of incident from the record
@@ -305,12 +305,12 @@
 
         // Calls from the library db
         "Library": {
-            id: '2ba0788a-2f35-43aa-a47c-89c75f55cf9d',
+            id: "2ba0788a-2f35-43aa-a47c-89c75f55cf9d",
             primaryFiltering: 'WHERE "Name" LIKE \'%OAKLAND%\'',
-            latLong: ['Lat', 'Lon'],
+            latLong: ["Lat", "Lon"],
             icon: iconTypes.LIBRARY_ICON,
 
-            title: (record) => record['Name'],
+            title: (record) => record["Name"],
             popup: (record) => `
               <strong>${record.Name}</strong>
               <br> Address: ${record.Address}
@@ -326,13 +326,13 @@
 
         },
         "Non-Traffic Violation": {
-            id: '6b11e87d-1216-463d-bbd3-37460e539d86',
+            id: "6b11e87d-1216-463d-bbd3-37460e539d86",
             primaryFiltering: 'Where "NEIGHBORHOOD" LIKE \'%Oakland\'',
-            latLong: ['Y', 'X'],
+            latLong: ["Y", "X"],
             icon: iconTypes.NON_TRAFFIC_VIOLATION,
 
-            title: (record) => record['OFFENSES'],
-            popup: (record) => record['OFFENSES'],
+            title: (record) => record["OFFENSES"],
+            popup: (record) => record["OFFENSES"],
 
             processRecord: (record) => {
                 record.incidentYear = parseInt(record.CITEDTIME.substring(0,4));
@@ -353,7 +353,7 @@
         let query = WPRDC_QUERY_PREFIX + dataSource.id + WPRDC_QUERY_SUFFIX + dataSource.primaryFiltering;
 
         if (options.limit) {
-          query += ' LIMIT ' + options.limit;
+          query += " LIMIT " + options.limit;
         }
 
         return fetch(WPRDC_BASE_URL + query)
@@ -367,13 +367,13 @@
             })
             .then((data) => {
                 if (!data || !data.result || !data.result.records) {
-                    displayNotification(`${dataSourceName} records not processed.`, 'error', (retryDiv) => {
-                        const retryButton = document.createElement('button');
+                    displayNotification(`${dataSourceName} records not processed.`, "error", (retryDiv) => {
+                        const retryButton = document.createElement("button");
                         retryButton.innerHTML = '<p><i class="fa fa-refresh" aria-hidden="true"></i> Retry</p>';
-                        retryButton.type = 'button';
-                        retryButton.className = 'retry';
-                        retryButton.addEventListener('click', function() {
-                            retryDiv.parentNode.style.display = 'none';
+                        retryButton.type = "button";
+                        retryButton.className = "retry";
+                        retryButton.addEventListener("click", function() {
+                            retryDiv.parentNode.style.display = "none";
                             fetchWPRDCData(dataSourceName);
                         });
                         retryDiv.appendChild(retryButton);
@@ -383,22 +383,22 @@
 
                 const records = data.result.records;
 
-                const filterContainer = document.createElement('div');
-                filterContainer.className = 'typeBtn';
+                const filterContainer = document.createElement("div");
+                filterContainer.className = "typeBtn";
 
-                const filter = document.createElement('input');
-                filter.id = dataSourceName.toLowerCase() + 'Check';
-                filter.type = 'checkbox';
+                const filter = document.createElement("input");
+                filter.id = dataSourceName.toLowerCase() + "Check";
+                filter.type = "checkbox";
                 filter.checked = true;
 
-                const filterLabel = document.createElement('label');
-                filterLabel.htmlFor = dataSourceName.toLowerCase() + 'Check';
-                filterLabel.innerHTML = dataSource.icon.options.html + ' - ' +
+                const filterLabel = document.createElement("label");
+                filterLabel.htmlFor = dataSourceName.toLowerCase() + "Check";
+                filterLabel.innerHTML = dataSource.icon.options.html + " - " +
                     dataSourceName;
 
-                filter.addEventListener('click', filterDisplay);
+                filter.addEventListener("click", filterDisplay);
 
-                document.getElementById('typeSelection').appendChild(filterContainer);
+                document.getElementById("typeSelection").appendChild(filterContainer);
                 filterContainer.appendChild(filter);
                 filterContainer.appendChild(filterLabel);
 
@@ -439,13 +439,13 @@
                     markers.push(record);
                 });
             })
-            .catch((err) => displayNotification(err, 'error', (retryDiv) => {
-                const retryButton = document.createElement('button');
+            .catch((err) => displayNotification(err, "error", (retryDiv) => {
+                const retryButton = document.createElement("button");
                 retryButton.innerHTML = '<p><i class="fa fa-refresh" aria-hidden="true"></i> Retry</p>';
-                retryButton.type = 'button';
-                retryButton.className = 'retry';
-                retryButton.addEventListener('click', function() {
-                    retryDiv.parentNode.style.display = 'none';
+                retryButton.type = "button";
+                retryButton.className = "retry";
+                retryButton.addEventListener("click", function() {
+                    retryDiv.parentNode.style.display = "none";
                     fetchWPRDCData(dataSourceName);
                 });
                 retryDiv.appendChild(retryButton);
@@ -454,20 +454,20 @@
 
     function fetchAllData() {
         Promise.all([
-            fetchWPRDCData('Police', { limit: 250 }),
-            fetchWPRDCData('311', { limit: 250 }),
-            fetchWPRDCData('Arrest', { limit: 250 }),
-            fetchWPRDCData('Code Violation', { limit: 250 }),
-            fetchWPRDCData('Library'),
-            fetchWPRDCData('Non-Traffic Violation', { limit: 250 })
+            fetchWPRDCData("Police", { limit: 250 }),
+            fetchWPRDCData("311", { limit: 250 }),
+            fetchWPRDCData("Arrest", { limit: 250 }),
+            fetchWPRDCData("Code Violation", { limit: 250 }),
+            fetchWPRDCData("Library"),
+            fetchWPRDCData("Non-Traffic Violation", { limit: 250 })
         ]).catch((err) => {
-            displayNotification(err, 'error', (retryDiv) => {
-                var retryButton = document.createElement('button');
+            displayNotification(err, "error", (retryDiv) => {
+                var retryButton = document.createElement("button");
                 retryButton.innerHTML = '<p><i class="fa fa-refresh" aria-hidden="true"></i> Retry</p>';
-                retryButton.type = 'button';
-                retryButton.className = 'retry';
-                retryButton.addEventListener('click', function() {
-                    retryDiv.parentNode.style.display = 'none';
+                retryButton.type = "button";
+                retryButton.className = "retry";
+                retryButton.addEventListener("click", function() {
+                    retryDiv.parentNode.style.display = "none";
                     fetchAllData();
                 });
                 retryDiv.appendChild(retryButton);
@@ -482,4 +482,4 @@
         return Math.floor(Math.abs(dateA.getTime() - dateB.getTime()) / 86400000);
     }
 
-})(typeof window !== 'undefined' ? window : {});
+})(typeof window !== "undefined" ? window : {});
