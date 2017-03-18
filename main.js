c43bf8608c0d451499ea037019ac5f53e5b89dab
Sam Nigh <spn11@pitt.edu>
2017-03-18 18:36:20 -0400

    TODO get full lab URL

--- main.js
+++ main.js
@@ -331,7 +331,10 @@
     //May not need the "options" parameter, as I can't see any data request from Pitt being overwhelmingly large
     function fetchPittData(dataSection, dataSourceName, filterCreated, options = {}){
         if (dataSection == "Labs"){
-            return fetch("http://securitas.cs.pitt.edu/lab_status/" + dataSourceName.toUpperCase())
+
+            //TODO: GET THE FULL URL WHEN THE SERVER IS UP |
+            //                                             V
+            return fetch("http://securitas.cs.pitt.edu/something/lab_status/" + dataSourceName.toUpperCase())
             .then((response) => {
                 // Inspired by https://github.com/github/fetch#handling-http-error-statuses
                 if (response.status >= 200 && response.status < 300) {
